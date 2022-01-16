f8c8c4a435da843dc82639e9a0c3ae75310f73d0
nafisa-tabassum-shouty <97848365+nafisa-tabassum-shouty@users.noreply.github.com>
2022-01-17 00:24:50 +0600

    Add files via upload

---
+++ Untitled2.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{int n,row,col;
+scanf("%d",&n);
+for(row=1;row<=n;row++)
+{
+    for(col=1;col<=n;col++)
+    {if(row==col || col==(n-(row-1)))
+        printf("#");
+        else
+        printf(" ");
+    }
+    printf("\n");
+}
+
+
+    return 0;
+}
